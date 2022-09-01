832a2eb280278ec3c6714ee3d04322a720f8e746
Lena Chu <lena.chu@Lena-Chu.local>
2022-09-01 16:26:16 -0500

    Finished the hamburger menu button

--- src/Components/MainNav/MainNav.js
+++ src/Components/MainNav/MainNav.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./main-nav.css";
 
 export default function MainNav() {
-  return <nav className="main-nav__container"></nav>;
+  const [checked, setChecked] = useState(true);
+
+  const handleToggle = (e) => {
+    setChecked(e.target.checked);
+  };
+  return (
+    <nav
+      className={checked ? "main-nav__container" : "main-nav__container folded"}
+    >
+      <div className="hamburger-menu">
+        <input
+          type="checkbox"
+          onChange={handleToggle}
+          checked={checked}
+        ></input>
+        <span className="first-child"></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+  );
 }
